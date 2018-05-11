ca1bef8cf71dcba31bbea7c32b47027246bb09b6
seohasong <tisutoo@gmail.com>
2018-05-12 03:08:00 +0900

    update origin master

--- module/load_data.py
+++
@@ -1,7 +0,0 @@
-
-from aws_handler import S3
-
-
-s3 = S3("seohasong", "./key/public-key.csv")
-data = s3.get_key("public/imdb/master")
-open("./data/imdb_master_df", "wb").write(data)
--- set-up.py
+++ set-up.py
@@ -1,4 +1,9 @@
 
-# Order is important.
+# The order is important.
 from module import load_module
-from module import load_data
+from module.aws_handler import S3
+
+
+s3 = S3("seohasong", "./key/public-key.csv")
+data = s3.get_key("public/imdb/master")
+open("./data/imdb_master_df", "wb").write(data)
