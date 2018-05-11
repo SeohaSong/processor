d7ce4c44a0d28143c2001a95d57008077f14d66e
seohasong <tisutoo@gmail.com>
2018-05-12 03:31:51 +0900

    update origin master

--- set-up.py
+++ set-up.py
@@ -6,6 +6,7 @@ from module.aws_handler import S3
 
 if __name__ == "__main__":
     
+    print("[set-up.py] Loading IMDB data from AWS S3...")
     s3 = S3("seohasong", "./key/public-key.csv")
     data = s3.get_key("public/imdb/master")
     open("./data/imdb_master_df", "wb").write(data)
