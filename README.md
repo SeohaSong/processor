b6c9ba9bc9b107459025f094b0170ca08fcab543
seohasong <tisutoo@gmail.com>
2018-05-12 02:22:18 +0900

    update origin master

---
+++ README.md
@@ -0,0 +1,9 @@
+# 파일 실행 순서
+
+1. 
+```bash
+$ python initiate.py
+$ python load-data.py
+```
+
+2. main.ipynb
---
+++ initiate.py
@@ -0,0 +1,12 @@
+
+import os
+
+
+if __name__ == "__main__":
+
+    cmd = """
+    git submodule init
+    git submodule update
+    """
+
+    os.system(cmd)
